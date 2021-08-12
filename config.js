93a899afa32d77b07bfb04209cde08b13075763a
hungtruongloveu <88787325+hungtruongloveu@users.noreply.github.com>
2021-08-12 09:43:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
     introTitle: 'xin chào Hà Thị Hương Giang hồ =))!',
-    introDesc: `Tớ có điều này muốn hỏi cậu,không trả lời cũng phải trả lời `,
+    introDesc: `Tớ có điều này muốn hỏi cậu, không trả lời cũng phải trả lời `,
     btnIntro: 'oke',
     title: 'Có thích siêu Anh Hùng không? 😙',
     desc: 'không trả lời hoặc thoát ra, tức là có đó nha :> ',
-    btnYes: 'thích ơi là thích <33',
+    btnYes: 'Dạ có ạ <33',
     btnNo: 'đéo :))',
     question:'Nói cho siêu Anh Hùng lý do vì sao thích đê <3',
-    btnReply: 'Gửi cho ANh Hùng <3',
-    reply: 'Tại vì đã đẹp trai lại còn cao m7 hihi =)))',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Tại vì đẹp trai lại còn cao m7 hihi =)))',
     mess: 'Tớ biết mà 🥰. Love you 3000 😘😘',
-    messDesc: 'còn giờ thì chờ gì nữa mà không inbox hát cho tui đi =).',
+    messDesc: 'còn giờ thì chờ gì nữa mà không inbox hát cho tui nghe =).',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/lazyeternal' 
 }
